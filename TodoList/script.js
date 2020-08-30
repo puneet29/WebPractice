0e748fd74cc2bc86ca31b0252caffd1f12eb73b9
puneet29 <puneet29saini@gmail.com>
2020-08-30 20:44:48 +0530

    Fixed alignment in todo

--- TodoList/script.js
+++ TodoList/script.js
@@ -11,16 +11,14 @@ function update() {
     for (let i = 0; i < tasks.length; i++) {
         if (tasks[i])
             new_html += `
-        <div class="row ml-5">
-            <div class="input-group mb-3 col-5">
+            <div class="input-group mb-3">
                 <div class="input-group-prepend">
                     <div class="input-group-text bg-secondary">
                         <input type="checkbox" onclick="task_done('`+ i + `')">
                     </div>
                 </div>
                 <input type="text" value="`+ tasks[i] + `" class="form-control bg-white" disabled>
-            </div>
-        </div>`;
+            </div>`;
     }
     document.getElementById("free-space").innerHTML = new_html;
 }
